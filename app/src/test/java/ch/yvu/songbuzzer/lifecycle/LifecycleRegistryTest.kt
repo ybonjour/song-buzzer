750298e9c6417a72374951f8fd99e159282554b4
Yves Bonjour <yves.bonjour@ergon.ch>
2017-12-16 10:47:02 +0100

    Adds more tests for LifecycleRegistry

--- app/src/test/java/ch/yvu/songbuzzer/lifecycle/LifecycleRegistryTest.kt
+++ app/src/test/java/ch/yvu/songbuzzer/lifecycle/LifecycleRegistryTest.kt
@@ -2,14 +2,21 @@ package ch.yvu.songbuzzer.lifecycle
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import org.junit.Before
 import org.junit.Test
 
 class LifecycleRegistryTest {
 
+    private lateinit var lifecycleRegistry: LifecycleRegistry
+
+    @Before
+    fun setUp() {
+        lifecycleRegistry = LifecycleRegistry()
+    }
+
     @Test
     fun notifiesRegisteredListenerOnResume() {
         val listener: LifecycleListener = mock()
-        val lifecycleRegistry = LifecycleRegistry()
         lifecycleRegistry.register(listener)
 
         lifecycleRegistry.onResume()
@@ -17,4 +24,21 @@ class LifecycleRegistryTest {
         verify(listener).onResume()
     }
 
+    @Test
+    fun notifiesAllRegisteredListeners() {
+        val listener1: LifecycleListener = mock()
+        val listener2: LifecycleListener = mock()
+        lifecycleRegistry.register(listener1)
+        lifecycleRegistry.register(listener2)
+
+        lifecycleRegistry.onResume()
+
+        verify(listener1).onResume()
+        verify(listener2).onResume()
+    }
+
+    @Test
+    fun doesNotThrowAnExceptionIfNoListenerIsRegistered() {
+        lifecycleRegistry.onResume()
+    }
 }
