3752e280f709d3977efe2b088e2af0cd9e940516
Yves Bonjour <yves.bonjour@ergon.ch>
2017-12-16 12:35:51 +0100

    Introduces inject template method

--- app/src/main/java/ch/yvu/songbuzzer/MainActivity.kt
+++ app/src/main/java/ch/yvu/songbuzzer/MainActivity.kt
@@ -1,18 +1,18 @@
 package ch.yvu.songbuzzer
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : PresentingActivity() {
     @Inject
     lateinit var mainPresenter: MainPresenter
+
+    override fun inject(component: ApplicationComponent) {
+        component.inject(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        SongBuzzerApplication.applicationComponent.inject(this)
     }
 }
--- app/src/main/java/ch/yvu/songbuzzer/PresentingActivity.kt
+++ app/src/main/java/ch/yvu/songbuzzer/PresentingActivity.kt
@@ -1,5 +1,14 @@
 package ch.yvu.songbuzzer
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-abstract class PresentingActivity : AppCompatActivity()
+abstract class PresentingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inject(SongBuzzerApplication.applicationComponent)
+    }
+
+    abstract fun inject(component: ApplicationComponent)
+}
